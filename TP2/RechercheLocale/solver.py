3bf8764f5a2a72a608f4fc0d048ad288394e49db
chuxiangsu <chu-xiang.su@polymtl.ca>
2023-10-21 16:11:19 -0400

    start tp-2

---
+++ TP2/RechercheLocale/solver.py
@@ -0,0 +1,17 @@
+from uflp import UFLP
+from typing import List, Tuple
+
+def solve(problem: UFLP) -> Tuple[List[int], List[int]]:
+    """
+    Votre implementation, doit resoudre le probleme via recherche locale.
+
+    Args:
+        problem (UFLP): L'instance du probleme à résoudre
+
+    Returns:
+        Tuple[List[int], List[int]]: 
+        La premiere valeur est une liste représentant les stations principales ouvertes au format [0, 1, 0] qui indique que seule la station 1 est ouverte
+        La seconde valeur est une liste représentant les associations des stations satellites au format [1 , 4] qui indique que la premiere station est associée à la station pricipale d'indice 1 et la deuxieme à celle d'indice 4
+    """
+    return None, None
+
